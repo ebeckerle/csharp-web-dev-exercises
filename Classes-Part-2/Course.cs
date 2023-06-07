47ecda7bd4b75179b7feb677451c5e7ebaa2cc3b
Elizabeth <92605769+ebeckerle@users.noreply.github.com>
2023-06-07 14:17:56 -0500

    1st Commit

--- Classes-Part-2/Course.cs
+++ Classes-Part-2/Course.cs
@@ -14,6 +14,18 @@ namespace Classes_Part_2
 
         // TODO: Add your custom 'Equals' method here. Consider which fields should match in order to call two
         //  Course objects equal.
+
+        public override bool Equals(object obj)
+        {
+            return obj is Course course &&
+               Topic == course.Topic &&
+               EqualityComparer<Teacher>.Default.Equals(Instructor, course.Instructor);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Topic, Instructor);
+        }
     }
 }
 
